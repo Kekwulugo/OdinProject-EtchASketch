ce5509d8db7d3f2e7017e276c640dd2d24fecfaa
Kekwulugo <93893923+Kekwulugo@users.noreply.github.com>
2024-03-08 15:04:33 -0700

    Fix mouseover event listener

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+// Get elements from DOM
 const button = document.querySelector(".btn");
 const div = document.querySelector("div");
+let container = document.querySelector(".container");
 
 let num = "";
 
 button.addEventListener("click", function(){
 
- num = prompt("How big would like the grid to be?");
+ num = prompt("How big would you like the grid to be?");
+
+ // clear previous grid
+ container.innerHTML = "";
  
- let container = document.querySelector(".container");
+ // Create cells for number indicated by user
  
  for (let i = 1; i<=num*num; i++){
   let newDiv = document.createElement("div");
@@ -18,11 +23,18 @@ button.addEventListener("click", function(){
 
 });
 
-//not working
+// Add hover affect, by adding a hover class
 
-div.childNodes.forEach(item => item.addEventListener("onmouseover",function (){
+const gridDivs = document.querySelectorAll(".ndiv");
+Array.from(gridDivs).forEach(item => item.addEventListener("onmouseover",function(){
+  item.classList.add("hover");
+}))
+
+/*div.childNodes.forEach(item => item.addEventListener("onmouseover",function (){
  item.style.backgroundColor = "yellow";
-}));
+}));*/
+
+
 
 
 
