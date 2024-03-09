39b03deb5c698f3e16573286728c82e5c5998a2f
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-03-09 16:43:34 -0700

    Complete Project

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 let button = document.querySelector(".btn");
 let container = document.querySelector(".container");
 
-let num = "";
 
+let num = "";
 function createGrid () {
 
   
   num = prompt("Please indicate the dimensions of the grid - ? X ?")
 
-  //container.style.border = "1px solid black";
   container.innerHTML = "";
-  container.style.width= "960px";
+  container.style.width = "960px";
+  container.style.border = "1px solid black";
 
 
   //determine size of cells
@@ -19,24 +19,29 @@ function createGrid () {
   
 
 // create cells
-  for (let i = 1; i<=num*num; i++){
-  let newDiv = document.createElement("div");
-  newDiv.setAttribute("class","ndiv");
-  newDiv.style.width = size + "px";
-  newDiv.style.height = size + "px";
-  //newDiv.style.border = "1px solid black";
-  
+  for (let i = 0; i < num; i++){
+    for (let j = 0; j < num; j++){
+      let newDiv = document.createElement("div");
+      newDiv.setAttribute("class","ndiv");
+      newDiv.style.width = size + "px";
+      newDiv.style.height = size + "px";
+      container.appendChild(newDiv);
 
-  //setup event listener for cells
-  newDiv.addEventListener("mouseover",() => {
-    newDiv.classList.add("hover");
+
+      //setup event listener for cells
+      newDiv.addEventListener("mouseover",() => {
+      randomColor = Math.floor(Math.random()*16777215).toString(16);
+      newDiv.style.backgroundColor = "#" + randomColor;
+        
+
+      
   })
 
-
-  container.appendChild(newDiv);
-
+    }
+  }
+  
 };
-}
+
 
 // user clicks button to generate grid
 button.onclick = createGrid;
